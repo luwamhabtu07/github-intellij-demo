262208ccf2f6f9c567e515cc0220f83abba9b83b
luwamhabtu07 <luwamhabtu07@gmail.com>
2025-01-11 23:26:42 -0800

    in it commit

---
+++ src/test/java/com/example/githubintellijdemo/GithubIntellijDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubintellijdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntellijDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
