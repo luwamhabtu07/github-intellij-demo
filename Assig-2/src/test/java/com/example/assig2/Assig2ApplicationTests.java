7e6482660590ff3d9c85ee19df2a7d951ff6754b
luwamhabtu07 <luwamhabtu07@gmail.com>
2025-01-20 11:11:38 -0800

    # Geometry Application
    This Java application calculates the area and perimeter of geometric shapes (Circle, Rectangle, and Triangle) using abstract classes and inheritance.
    
    ## Features
    - Implements an abstract `Shape` class with concrete and abstract methods.
    - Supports Circle, Rectangle, and Triangle shapes with relevant calculations.
    - Demonstrates inheritance and method overriding in Java.
    
    ## How to Run
    1. Clone the repository:
       ```bash
       git clone https://github.com/yourusername/GeometryApp.git

---
+++ Assig-2/src/test/java/com/example/assig2/Assig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ assig-3/src/main/java/com/example/assig3/Assig3Application.java
@@ -0,0 +1,13 @@
+package com.example.assig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assig3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assig3Application.class, args);
+    }
+
+}
